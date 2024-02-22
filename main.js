d70fd6221ddf0aada6d6e950a033021dc55830c9
Jelani Woods <jelani@firstdraft.com>
2024-02-21 18:16:59 -0600

    Add initial main configuration

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require("electron")
+const path = require("node:path")
+
+const createWindow = () => {
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  window.loadFile("index.html")
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  // Open a window if none are open (macOS)
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit the app when all windows are closed (Windows & Linux)
+app.on("window-all-closed", () => {
+  // darwin, meaning macOS
+  if (process.platform !== "darwin") app.quit()
+})
+
