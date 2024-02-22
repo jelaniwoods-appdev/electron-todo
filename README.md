59483ac6ceabd45f34788ac311a822f5186c37c5
Jelani Woods <jelani@firstdraft.com>
2024-02-21 19:20:41 -0600

    Notes

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Electron TODO
 
 Simple todo app for practice.
+
+## Notes
+
+You can interact with this web content from the main process using the window's webContents object.
+```js
+const win = new BrowserWindow({ width: 800, height: 600 })
+win.loadURL("https://github.com")
+
+const contents = win.webContents
+```
+
+The bare minimum to understand is:
+
+> An HTML file is your entry point for the renderer process.
+> UI styling is added through Cascading Style Sheets (CSS).
+> Executable JavaScript code can be added through <script> elements.
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow } = require("electron")
 const path = require("node:path")
 
+
+
 const createWindow = () => {
   const window = new BrowserWindow({
     width: 800,
@@ -12,6 +14,11 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow()
+  const win = new BrowserWindow({ width: 800, height: 600 })
+  win.loadURL("https://github.com")
+  
+  const contents = win.webContents
+  console.log(contents)
   // Open a window if none are open (macOS)
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
